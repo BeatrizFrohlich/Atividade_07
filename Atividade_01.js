46ccea2f291ea1f7ac2fee7c3ac8075ea0de0218
Beatriz Fröhlich <beatriz_frohlich@estudante.sesisenai.org.br>
2026-03-13 09:34:19 -0300

    ADD - Atividade 01

---
+++ Atividade_01.js
@@ -0,0 +1,23 @@
+function SomarNumeros(){
+    let soma = 0;
+    let continuar = true;
+
+    while(continuar){
+        let numero = parseFloat(prompt("Digite um numero (ou digite '0' para encerrar):"));
+
+        //soma = soma + numero;
+        //soma += numero;
+
+        if(isNaN(numero)){
+            alert("Por favor, digite um numero valido!");
+        } else{
+            soma += numero;
+            console.log(soma);
+        }
+
+        continuar = confirm("Deseja adicionar mais um numero?");
+    }
+    alert("A Soma dos numeros é:" + soma);
+}
+
+SomarNumeros();
