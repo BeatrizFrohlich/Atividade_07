925c0e984c2911c3c675fd3343890ae90ccad1cc
Beatriz Fröhlich <beatriz_frohlich@estudante.sesisenai.org.br>
2026-03-13 10:57:05 -0300

    Add - Atividade 03

--- script.js
+++ script.js
@@ -31,3 +31,33 @@ function atividade02(){
 }
 
 //atividade02()
+
+function atividade03(){
+    let ganhoAnual = 0;
+    let gastoAnual = 0;
+
+    for (let i = 1; i <= 12; i++) {
+
+    let ganho = parseFloat(prompt("Digite o ganho bruto do mês " + i + ":"));
+    let gasto = parseFloat(prompt("Digite os gastos do mês " + i + ":"));
+
+    ganhoAnual = ganhoAnual + ganho;
+    gastoAnual = gastoAnual + gasto;
+}
+
+    let saldo = ganhoAnual - gastoAnual;
+
+    console.log("Ganho bruto anual: " + ganhoAnual);
+    console.log("Gasto anual: " + gastoAnual);
+    console.log("Saldo financeiro anual: " + saldo);
+
+    if (saldo > 0) {
+    console.log("A empresa teve Lucro.");
+    } else if (saldo < 0) {
+    console.log("A empresa teve Prejuízo.");
+    } else {
+    console.log("A empresa ficou no zero a zero.");
+}
+}
+
+//atividade03()
