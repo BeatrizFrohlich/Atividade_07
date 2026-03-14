aadbb30f86f9ed1afcf30db55edacaf554cc73ee
BeatrizFrohlich <beatriz_frohlich@estudante.sesisenai.org.br>
2026-03-14 20:11:35 -0300

    Add - Atividade 09

--- script.js
+++ script.js
@@ -192,3 +192,20 @@ function atividade08(){
 }
 
 //atividade08()
+
+function atividade09(){
+    let num1 = parseInt(prompt("Digite o primeiro número inteiro:"));
+    console.log("Primeiro número digitado:", num1);
+
+    let num2 = parseInt(prompt("Digite o segundo número inteiro:"));
+    console.log("Segundo número digitado:", num2);
+
+    if (num1 > num2) {
+        console.log("Ordem do maior para o menor:", num1, ",", num2);
+    } else {
+        console.log("Ordem do maior para o menor:", num2, ",", num1);
+    }
+
+}
+
+atividade09()
